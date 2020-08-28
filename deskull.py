5b152803af55d7cffe548fd596a9881631d08c93
Patrick Rock <prock2011@gmail.com>
2020-08-28 02:30:39 -0500

    skull stripping

--- deskull.py
+++ deskull.py
@@ -1,40 +1,57 @@
 import numpy as np
 from skimage import measure
+from skimage import filters 
+
 import nibabel as nib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import sys
 import os
 
+
 def get_axial(voxels, n):
+    # reformats matrix so first index pulls axials     
     axial = [] 
     for sl in voxels:
        axial.append([x[n] for x in sl])
     return axial
 
+def strip_skull(voxels, hu=100):
+    def f(x):
+        if x>hu:
+            return -1000
+        else:
+            return x
+    return np.vectorize(f)(voxels)
+
+def animate(data):
+    fig = plt.figure()
+    ims = []
+    for i in range(66):
+        im = plt.imshow(get_axial(data, i), animated=True)
+        ims.append([im])
+    ani = animation.ArtistAnimation(fig, ims, interval=80, blit=True,
+                                    repeat_delay=1000)
+    plt.show()
+
+
 if len(sys.argv) == 1:
     level = 200
-
-
 else:
     level = int(sys.argv[1])
 
+
+
+
 img = nib.load('l1_Orig.nii')
 data = img.get_fdata()
-windowed_data = 1*(data > level)
+windowed_data = strip_skull(data)
 bones = nib.Nifti1Image(windowed_data, img.affine, img.header)
 nib.save(bones, 'bones.nii')
+
+
 all_labels = measure.label(windowed_data)
 
-fig = plt.figure()
-ims = []
-for i in range(66):
-    im = plt.imshow(get_axial(all_labels, i), cmap='nipy_spectral', animated=True)
-    ims.append([im])
-
-ani = animation.ArtistAnimation(fig, ims, interval=80, blit=True,
-                                repeat_delay=1000)
-plt.show()
 
 #os.system('miview bones.nii')
 
