adae9d81339618ca4db42b252b5a783a6d491b84
Patrick Rock <prock2011@gmail.com>
2020-08-28 03:38:06 -0500

    working on largest connected component

--- deskull.py
+++ deskull.py
@@ -1,7 +1,7 @@
 import numpy as np
 from skimage import measure
 from skimage import filters 
-
+from scipy import stats
 import nibabel as nib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -11,44 +11,45 @@ import os
 # consider connected componenets only in two dimensions
 # do multiple orthogonal passes and throw out the small components 
 
+# should voxels have a type? they can keep track of what cc they are in
+
 np.set_printoptions(threshold=sys.maxsize)
 
 def get_axial(voxels, n):
     # reformats matrix so first index pulls axials     
     axial = [] 
-    for sl in voxels:
-       axial.append([x[n] for x in sl])
+    for vs in voxels:
+       axial.append([v[n] for v in vs])
     return axial
 
 def strip_range(voxels, upper=75, lower=0):
-    def f(x):
-        if x<lower or x>upper:
+    def f(v):
+        if v<lower or v>upper:
             return 0
         else:
-            return x
+            return v
     return np.vectorize(f)(voxels)
 
 def connected_components(voxels):
-    def f(x):
-        if x < 1: 
+    def f(v):
+        if v < 1: 
             return 0
         else:
-            return x
-    #voxels = filters.gaussian(voxels, sigma = 200) 
+            return 1
     blobs = np.vectorize(f)(voxels)
-
-    # remove this
-    b = nib.Nifti1Image(blobs, img.affine, img.header)
-    nib.save(b, 'blobs.nii')
-
-    v = nib.Nifti1Image(voxels, img.affine, img.header)
-    nib.save(v, 'voxels.nii')
-
-
     m = measure.label(blobs)
-    print(m)
     return m
 
+def biggest_component(ccs):
+    ccs = ccs.flatten()
+    ccs = ccs[ccs != 0] # remove 0s
+    return stats.mode(ccs)[0][0]
+
+def toss_small_components(voxels):
+    # sets all voxels to zero that arent in the largest connected component
+    ccs = connected_components(voxels)
+    b = biggest_component(ccs)
+    
 
 def animate(data):
     fig = plt.figure()
@@ -74,8 +75,9 @@ strip = nib.Nifti1Image(stripped_data, img.affine, img.header)
 nib.save(strip, 'strip.nii')
 
 
-animate(connected_components(stripped_data))
-
+cc = connected_components(get_axial(stripped_data,30))
+plt.imshow(cc, cmap='nipy_spectral')
+plt.show()
 
 #os.system('miview bones.nii')
 
